945122731892a64d237272d4dba5dacabc315555
Jordi Fernández <jordi.fernandez@karveinformatica.com>
2017-04-19 18:35:34 +0200

    Ejemplo Hosting Wpf un Windows Form

---
+++ Training/WpfTraining/WpfTraining/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTraining
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //<snippet10>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Create the interop host control.
+            System.Windows.Forms.Integration.WindowsFormsHost host =
+                new System.Windows.Forms.Integration.WindowsFormsHost();
+
+            // Create the MaskedTextBox control.
+            MaskedTextBox mtbDate = new MaskedTextBox(DateTime.Now.ToString());
+
+            // Assign the MaskedTextBox control as the host control's child.
+            host.Child = mtbDate;
+
+            // Add the interop host control to the Grid
+            // control's collection of child controls.
+            this.grid1.Children.Add(host);
+        }
+        //</snippet10>
+    }
+}
+
