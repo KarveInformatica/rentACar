91837a654e54c22a6edef0deb07bc775b90b58a1
giorgio.zoppi@gmail.com <giorgio.zoppi@gmail.com>
2017-05-21 13:16:33 +0200

    First inception Splash

---
+++ Sources/RentACarNG/SplashScreenModule/Loader.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Threading;
+
+namespace Karve.SplashScreenModule
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Loader
+    {
+        private static Application App;
+
+        public Loader(Application app)
+        {
+            App = app;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        [STAThread ( )]
+        static void Main ( )
+        {
+            Splasher.Splash = new NewSplashScreen ( );
+            Splasher.ShowSplash();
+
+            for ( int i = 0; i < 1000; i++ )
+            {
+                MessageListener.Instance.ReceiveMessage ( string.Format ( "Load module {0}", i ) );
+                Thread.Sleep ( 1 );
+            }
+            App.MainWindow.Show();
+        }
+    
+     
+    }
+}
