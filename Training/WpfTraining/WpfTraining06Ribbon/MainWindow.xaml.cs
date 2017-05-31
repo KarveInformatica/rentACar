b32959fa6f7e9de6c8cc69e22fbb8c7a6c10fc4e
Jordi Fernández <jordi.fernandez@karveinformatica.com>
2017-05-31 10:11:49 +0200

    www . wpf-tutorial . com

--- Training/WpfTraining/WpfTraining06Ribbon/MainWindow.xaml.cs
+++ Training/WpfTraining/WpfTraining06Ribbon/MainWindow.xaml.cs
@@ -1,28 +1,25 @@
 ﻿using Microsoft.Windows.Controls.Ribbon;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
+
+
+using System.Collections.ObjectModel;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Threading;
+using System.Globalization;
 
 namespace WpfTraining06Ribbon
 {
     /// <summary>
     /// Lógica de interacción para MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : RibbonWindow
     {
         public MainWindow()
         {
+            this.WindowState = WindowState.Maximized;
             InitializeComponent();
         }
         #region ToggleButton collapse/Expand
